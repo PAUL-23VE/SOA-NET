0bb13e19e8fce23ba39d83c7e3ce35f9d19516c4
xxxMichael <mayckolchavez27@gmail.com>
2025-09-10 00:12:03 -0500

    se agrega el modelo de cliente y se crea el controlador

---
+++ ClienteService/Controllers/ClientesController.cs
@@ -0,0 +1,16 @@
+﻿using ClienteService.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClienteService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : Controller
+    {
+        /* public IActionResult Index()
+         {
+             return View();
+         }
+       */
+    }
+}
--- ClienteService/obj/Release/net8.0/ClienteService.AssemblyInfo.cs
+++ ClienteService/obj/Release/net8.0/ClienteService.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClienteService")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8290214968fb057d48ff2c0bdd9e67c6637f0aae")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClienteService")]
